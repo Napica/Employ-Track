158fb66a6cf85fa02998cec2326c057cf3132841
Archangel <nathapcal@gmail.com>
2020-10-06 03:20:53 -0400

    added addEmployee function.  Able to gather information

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ function startFunction() {
       choices: [
         "View All Employees",
         "View All Employees by Department",
+        "Add Employee",
         "EXIT",
       ],
     })
@@ -36,14 +37,17 @@ function startFunction() {
         viewAllEmployees();
       } else if (answer.start === "View All Employees by Department") {
         viewEmployeesByDepartment();
+      } else if (answer.start === "Add Employee") {
+        // console.log("this was chosen")
+        addEmployee();
       } else if (answer.start === "EXIT") {
         connection.end();
       }
     });
 }
 
+// function to view all employees
 function viewAllEmployees() {
-  //   console.log("you can see this");
   connection.query(
     "SELECT first_name, last_name, title, salary, name FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN departments On employee.departments_id = departments.id",
     (err, data) => {
@@ -54,15 +58,14 @@ function viewAllEmployees() {
   );
 }
 
+// Function to view employees by department
 function viewEmployeesByDepartment() {
   connection.query("SELECT name FROM departments", (err, data) => {
     if (err) throw err;
-    // console.log(data)
     const departmentSelection = [];
     for (let i = 0; i < data.length; i++) {
       departmentSelection.push(data[i]);
     }
-    // console.log(departmentSelection);
     inquirer
       .prompt({
         name: "selection",
@@ -71,16 +74,49 @@ function viewEmployeesByDepartment() {
         choices: departmentSelection,
       })
       .then(({ selection }) => {
-        // console.log(selection);
         connection.query(
           "SELECT first_name, last_name, role.title, departments.name FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN departments ON role.departments_id=departments.id WHERE departments.name = ?;",
           [selection],
           (err, res) => {
             if (err) throw err;
             console.table(res);
-            startFunction()
+            startFunction();
           }
         );
+      });
+  });
+}
+
+function addEmployee() {
+  connection.query("SELECT title FROM role", (err, data) => {
+    if (err) throw err;
+    const titleSelection = [];
+    for (let i = 0; i < data.length; i++) {
+      titleSelection.push(data[i]);
+    //   console.log(data[i])
+    }
+    // console.log(titleSelection);
+    inquirer
+      .prompt([
+        {
+          name: "firstName",
+          type: "input",
+          message: "What is the employee's first name?",
+        },
+        {
+          name: "lastName",
+          type: "input",
+          message: "What is the employee's last name?",
+        },
+        {
+          name: "position",
+          type: "list",
+          message: "What is the employee's role?",
+          choices: titleSelection,
+        },
+      ])
+      .then(({ firstName, lastName, position }) => {
+        console.log(firstName, lastName, position);
       });
   });
 }
